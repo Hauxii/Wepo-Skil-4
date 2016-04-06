45d2c40533792d08277fa421977a619b3f2e9278
Hauxii <haukur91@gmail.com>
2016-04-06 12:40:37 +0000

    fixed top boundries

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -103,10 +103,10 @@ window.Player = (function() {
 	};
 
 	Player.prototype.checkCollisionWithBounds = function() {
-		if (this.pos.x < 0 ||
-			this.pos.x + WIDTH > this.game.WORLD_WIDTH ||
-			this.pos.y < 0 ||
-			this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
+		if(this.pos.y < 0){
+			this.pos.y = 0;
+		}
+		if (this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
 			document.getElementById('AudioMainPart').pause();
 			document.getElementById('DeathSound').currentTime = 0;
 			document.getElementById('DeathSound').play();
